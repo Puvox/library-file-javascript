1e607790b42c334adbd164d538721e9d16cc7d95
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-10-28 21:10:22 +0400

    -

--- library_standard.js
+++ library_standard.js
@@ -1466,8 +1466,8 @@ const puvox_library =
 		const suffix = 'm';
 		const objectTree = {
 			types: { reset: "0", bright: "1", dim: "2", underscore: "4", blink: "5", reverse: "7", hidden: "8", },
-			background: { black: "30", red: "31", green: "32", yellow: "33", blue: "34", magenta: "35", cyan: "36", white: "37", },
-			foreground: { black: "40", red: "41", green: "42", yellow: "43", blue: "44", magenta: "45", cyan: "46", white: "47", }
+			foreground: { black: "30", red: "31", green: "32", yellow: "33", blue: "34", magenta: "35", cyan: "36", white: "37", },
+			background: { black: "40", red: "41", green: "42", yellow: "43", blue: "44", magenta: "45", cyan: "46", white: "47", }
 		};
 		const bfKeyname = backgroundOrForeground ? 'background' : 'foreground';
 		console.log (prefix + objectTree[bfKeyname][color] + suffix + "%s" + prefix + objectTree.types.reset + suffix, text);
