57cdb0ffda0a480f3cfd6484c89320529352304a
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-11-27 13:54:07 +0400

    -

---
+++ library_standard.d.ts
@@ -0,0 +1,19 @@
+export = puvox_library;
+/**
+ *
+ *  ############################################################################
+ *  #############################  Our JS Library   ############################
+ *  ### Here we collect frequently used methods across our JS applications.  ###
+ *  ### (Some of them are generic javascript functions, some are for NodeJS) ###
+ *  ############################################################################
+ *
+ *  ########## Example usage: ##########
+ *  const helpers = new PuvoxLibrary();
+ *         console.log ( helpers.get_last_child_of_array(array) );
+ *         console.log ( helpers.get_visitor_ip() );
+ *         console.log ( helpers.telegramMessage("hello world", "1234567890", "BOTKEY123:456789") );
+ *       ... etc
+ *
+*/
+declare const puvox_library: any;
+//# sourceMappingURL=library_standard.d.ts.map
